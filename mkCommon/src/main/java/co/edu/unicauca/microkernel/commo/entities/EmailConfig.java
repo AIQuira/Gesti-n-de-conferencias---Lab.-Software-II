00c46842d4a1c41fed9e2e2234b3ee4e740c4fe5
thaliabernalepe <163876693+thaliabernalepe@users.noreply.github.com>
2024-09-29 09:07:41 -0500

    plugins mkCommon y mkPlugin
     plugins mkCommon y mkPlugin del microkernel notificaciones

---
+++ mkCommon/src/main/java/co/edu/unicauca/microkernel/commo/entities/EmailConfig.java
@@ -0,0 +1,32 @@
+
+package co.edu.unicauca.microkernel.commo.entities;
+
+/**
+ *
+ * @author thali
+ */
+public class EmailConfig {
+    private String username = "pruebalina25@hotmail.com"; // Tu cuenta de Hotmail
+    private String password = "redire25"; // Tu contraseña de Hotmail
+
+    public EmailConfig(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ mkCommon/src/main/java/co/edu/unicauca/microkernel/common/interfaces/ISendEmail.java
@@ -0,0 +1,10 @@
+
+package co.edu.unicauca.microkernel.common.interfaces;
+
+/**
+ *
+ * @author thali
+ */
+public interface ISendEmail {
+    void sendEmail(String username, String password, String addressee);
+}
