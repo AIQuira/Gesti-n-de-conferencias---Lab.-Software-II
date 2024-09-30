725780252aef2849e3007a26cda7d70f4857eb50
sonhuila <mariapaulabm09@unicauca.edu.co>
2024-09-30 07:52:52 -0500

    documentacion common

--- mkCommon/src/main/java/co/edu/unicauca/microkernel/commo/entities/EmailConfig.java
+++ mkCommon/src/main/java/co/edu/unicauca/microkernel/commo/entities/EmailConfig.java
@@ -1,31 +1,58 @@
-
 package co.edu.unicauca.microkernel.commo.entities;
 
 /**
- *
+ * Clase que representa la configuración del correo electrónico.
+ * Esta clase contiene la información necesaria para autenticar y configurar el acceso a una cuenta de correo.
+ * 
  * @author thali
  */
 public class EmailConfig {
-    private String username = "thaliaepe@hotmail.com"; // cuenta de Hotmail
-    private String password = "carolt12345"; // contraseña de Hotmail
+    private String username = "thaliaepe@hotmail.com"; // Cuenta de Hotmail
+    private String password = "carolt12345"; // Contraseña de Hotmail
 
+    /**
+     * Constructor que inicializa la configuración del correo electrónico.
+     * 
+     * @param username El nombre de usuario de la cuenta de correo.
+     * @param password La contraseña de la cuenta de correo.
+     */
     public EmailConfig(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    /**
+     * Obtiene el nombre de usuario de la cuenta de correo.
+     * 
+     * @return El nombre de usuario.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Establece el nombre de usuario de la cuenta de correo.
+     * 
+     * @param username El nuevo nombre de usuario.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Obtiene la contraseña de la cuenta de correo.
+     * 
+     * @return La contraseña.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Establece la contraseña de la cuenta de correo.
+     * 
+     * @param password La nueva contraseña.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
--- mkCommon/src/main/java/co/edu/unicauca/microkernel/common/interfaces/ISendEmail.java
+++ mkCommon/src/main/java/co/edu/unicauca/microkernel/common/interfaces/ISendEmail.java
@@ -1,10 +1,20 @@
-
 package co.edu.unicauca.microkernel.common.interfaces;
 
 /**
- *
+ * Interfaz que define el contrato para el envío de correos electrónicos.
+ * Cualquier clase que implemente esta interfaz deberá proporcionar
+ * una implementación del método para enviar correos.
+ * 
  * @author thali
  */
 public interface ISendEmail {
+    
+    /**
+     * Envía un correo electrónico a un destinatario específico.
+     * 
+     * @param username El nombre de usuario de la cuenta de correo que enviará el mensaje.
+     * @param password La contraseña de la cuenta de correo que enviará el mensaje.
+     * @param addressee La dirección de correo electrónico del destinatario.
+     */
     void sendEmail(String username, String password, String addressee);
 }
